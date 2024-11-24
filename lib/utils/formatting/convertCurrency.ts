c37ba2df04bceed5a9867109e13c70970b5f6eca
JosSainson <121938088+jos34000@users.noreply.github.com>
2024-11-24 21:26:55 +0100

    release(api): release all api routes

---
+++ lib/utils/formatting/convertCurrency.ts
@@ -0,0 +1,29 @@
+import { fetchBinancePrice } from "@/lib/api/binance/fetchFinancePrice";
+
+export const convertCurrency = async (
+  amount: string,
+  fromCurrency: string,
+  toCurrency: string
+): Promise<number> => {
+  try {
+    const priceInSource = parseFloat(amount.replace(",", "."));
+
+    if (fromCurrency === toCurrency)
+      return parseFloat(priceInSource.toFixed(6));
+
+    const isUsdtToEur = fromCurrency === "USDT" && toCurrency === "EUR";
+    const isEurToUsdt = fromCurrency === "EUR" && toCurrency === "USDT";
+
+    const eurUsdtRate = await fetchBinancePrice("EURUSDT");
+
+    if (isUsdtToEur)
+      return parseFloat((priceInSource / eurUsdtRate).toFixed(6));
+    if (isEurToUsdt)
+      return parseFloat((priceInSource * eurUsdtRate).toFixed(6));
+
+    const targetRate = await fetchBinancePrice(`EUR${toCurrency}`);
+    return parseFloat((priceInSource * targetRate).toFixed(6));
+  } catch (error) {
+    return NaN;
+  }
+};
